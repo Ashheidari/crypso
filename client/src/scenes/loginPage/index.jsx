344f510d849232f3d04cc1ccf5dcdef575a69fcc
Ashheidari <Heidariarash1@gmail.com>
2023-04-29 22:42:11 -0700

    login page and register page added

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,9 +1,35 @@
-
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from './Form'
 
 const LoginPage = () => {
-    return (
-      <div>LoginPage</div>
-    )
-  }
-  
-  export default LoginPage;
+  const theme = useTheme();
+  const isNonMobileScreen = useMediaQuery("(min-width : 1000px )");
+  return (
+    <Box>
+      <Box
+        width="100%"
+        p="1rem 6%"
+        backgroundColor={theme.palette.background.alt}
+        textAlign="center"
+      >
+        <Typography fontWeight="Bold" fontSize="32px" color="primary">
+          Crypso
+        </Typography>
+      </Box>
+      <Box
+        width={isNonMobileScreen ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Typography fontWeight="500" variant="h5" sx={{mb:"1.5rem"}} textAlign="center">
+          Welcome to crypso, community for crypto guys
+        </Typography>
+        <Form/>
+      </Box>
+    </Box>
+  );
+};
+
+export default LoginPage;
